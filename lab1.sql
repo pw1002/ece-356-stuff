51a2bac88dc5c6983e42541c54b49b8ae67dacb2
Phillip Wu <46137965+pw1002@users.noreply.github.com>
2022-09-26 20:58:22 -0400

    Add files via upload

---
+++ lab1.sql
@@ -0,0 +1,51 @@
+---- 1b
+/*
+Delete this block comment and place your answer to 1b here.
+*/
+
+---- 1e
+/*
+Delete this block comment and place your answer to 1e here.
+*/
+
+
+---- 1g
+/*
+Delete this block comment and place your answer to 1g here.
+*/
+
+
+---- 2a
+/*
+Delete this block comment and place your answer to 2a here.
+*/
+
+
+---- 2e
+/*
+Delete this block comment and place your answer to 2e here.
+*/
+
+
+---- 3a
+/*
+Delete this block comment and place your answer to 3a here.
+*/
+
+
+---- 3b
+/*
+Delete this block comment and place your answer to 3b here.
+*/
+
+
+---- 3c
+/*
+Delete this block comment and place your answer to 3c here.
+*/
+
+
+---- 3d
+/*
+Delete this block comment and place your answer to 3d here.
+*/
